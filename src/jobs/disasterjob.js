af94c48ac19b822583e1e8c761546d36cc24df65
rudraa-ksh <rudrak04sh@gmail.com>
2025-08-03 10:44:02 +0530

    Feat: Perform CRON jobs twice a day

--- src/jobs/disasterjob.js
+++ src/jobs/disasterjob.js
@@ -1,8 +1,8 @@
 import cron from "node-cron";
-import syncActiveDisaster from "../services/disasterServices";
+import syncActiveDisaster from "../services/disasterServices.js";
 
 export default async function checkDisasters(){
-    cron.schedule("0 0 * * * ",async ()=>{
+    cron.schedule("0 9,21 * * * ",async ()=>{
         await syncActiveDisaster();
     });
 }
